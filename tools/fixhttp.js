9ee79a4f67fb6d6f896499c5bb423e7c6a70b162
phaze-the-dumb <phaze@duck.com>
2023-07-20 13:42:42 +0100

    fuck fuck fuck fuck

--- tools/fixhttp.js
+++ tools/fixhttp.js
@@ -2,5 +2,5 @@ const records = require('../data/records.json');
 const fs = require('fs');
 const path = require('path');
 
-records.forEach(r => r.type === 'http');
+records.forEach((r, i) => records[i].type === 'http');
 fs.writeFileSync(path.join(__dirname, '../data/records.json'), JSON.stringify(records));
